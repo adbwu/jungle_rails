7306efdd8208899cc945d537d1fe6bfa8b4f163e
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-27 03:04:40 -0500

    added methods to the dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,19 @@
 class Admin::DashboardController < AdminController
   def show
   end
+
   def index
   end
+
+  def product_count
+    Product.count
+  end
+
+  def category_count
+    Category.count
+  end
+
+  def sold_out_products
+  end 
+
 end
