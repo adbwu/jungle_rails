ea526fe6ccc9f88cba7fa46371bb8e31a6523e6d
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-27 03:58:36 -0500

    add route to about

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
     post '/login' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
 
+    get '/about' => 'about#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
